bd8d70e2ec9f4c9a93b1c6c26f26a0506e9026b7
James Huang <jpluschh@gmail.com>
2020-12-13 04:44:51 +0800

    fix curl shell script bug

--- covid/raw-data/curl-raw.sh
+++ covid/raw-data/curl-raw.sh
@@ -2,5 +2,4 @@
 
 backup_dir=$(date +'%Y%m%d')
 mkdir $backup_dir
-curl -o $backup_dir/owid-covid-data.csv https://github.com/owid/covid-19-data/blob/master/public/data/owid-covid-data.csv
-curl -o $backup_dir/owid-covid-data.json https://github.com/owid/covid-19-data/blob/master/public/data/owid-covid-data.json
+curl -o $backup_dir/owid-covid-data.csv https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.csv
